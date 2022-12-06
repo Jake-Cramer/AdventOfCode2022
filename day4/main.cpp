96e2f411ca1fdeb0b31bb287eab94e3fa9921fb9
penguin-dude123 <73714848+penguin-dude123@users.noreply.github.com>
2022-12-05 20:59:50 -0500

    Day 4 optimization

--- day4/main.cpp
+++ day4/main.cpp
@@ -15,7 +15,6 @@ int contains(int nums[4]){
 }
 
 int overlap(int nums[4]){
-
 	if(nums[2] >= nums[0] && nums[2] <= nums[1])
 		return 1;
 	if(nums[3] >= nums[0] && nums[3] <= nums[1])
@@ -24,22 +23,22 @@ int overlap(int nums[4]){
 		return 1;
 	if(nums[1] >= nums[2] && nums[1] <= nums[3])
 		return 1;
-
 	return 0;
 }
 
 
 int main(){
 
-	int numbers[1000][4];
+	int numbers[4];
 	
 	std::ifstream infile("input.txt");
 
 	std::string temp;
-	
-	int i = 0;
-	while(std::getline(infile, temp)){
 
+	int count = 0;
+	int pt2 = 0;
+	
+	while(std::getline(infile, temp)){
 		int split = temp.find(",");
 		std::string pair1;
 		std::string pair2;
@@ -54,24 +53,14 @@ int main(){
 		std::string num3 = pair2.substr(0, splitB);
 		std::string num4 = pair2.substr(splitB + 1, -1);
 		
-		numbers[i][0] = std::stoi(num1);
-		numbers[i][1] = std::stoi(num2);
-		numbers[i][2] = std::stoi(num3);
-		numbers[i][3] = std::stoi(num4);
+		numbers[0] = std::stoi(num1);
+		numbers[1] = std::stoi(num2);
+		numbers[2] = std::stoi(num3);
+		numbers[3] = std::stoi(num4);
 
-		i++;
-	}
-
-	// all numbers are in numbers
-	
-	int count = 0;
-	int pt2 = 0;
-
-	// check if one range fully contains another
-	for(int i = 0; i < int(sizeof(numbers)/sizeof(numbers[i])); i++){	
-		if(contains(numbers[i]))
+		if(contains(numbers))
 			count++;
-		if(overlap(numbers[i]))
+		if(overlap(numbers))
 			pt2++;
 	}
 
